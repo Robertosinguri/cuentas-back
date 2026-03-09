783b80232561bd1c383b3b3515533aabd5058077
Roberto Singuri <robertosinguri@gmail.com>
2026-03-09 16:04:56 -0300

    chore: add firebase hosting to allowable cors origins

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "http://127.0.0.1:4200"],
+    allow_origins=[
+        "http://localhost:4200", 
+        "http://127.0.0.1:4200",
+        "https://cuentas-40610.web.app",
+        "https://cuentas-40610.firebaseapp.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
